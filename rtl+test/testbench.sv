3f56e3e163bf5a3c5cc510226e899a1cf951aec6
g-savant <42213922+g-savant@users.noreply.github.com>
2024-04-19 18:01:30 -0400

    started testbench

--- rtl+test/testbench.sv
+++ rtl+test/testbench.sv
@@ -2,5 +2,30 @@
 
 module test();
 
+  logic clk, rst;
+  logic ard_data_ready;
+  logic ard_receive_ready;
+  logic[7:0] in_bus;
+  logic data_in_ready;
+  logic data_out_ready, shift_done;
+  logic[7:0] out_bus;
+
+
+  cpu_core cpu(.*);
+
+  logic[7:0][15:0] instr_memory;
+
+  logic[7:0][15:0] data_memory;
+
+  initial begin
+    clk = 1'b0;
+    forever #200 clk = ~clk;
+  end
+
+
+
+  initial begin
+
+  end
 
 endmodule
